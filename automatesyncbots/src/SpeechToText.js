7c4ca1f3b694ee706e9701cdaaec9c5f914f98d6
surajpatra25 <surajpatra25@gmail.com>
2023-11-16 15:41:34 -0500

    changes to develop
    changes to develop

--- automatesyncbots/src/SpeechToText.js
+++ automatesyncbots/src/SpeechToText.js
@@ -38,7 +38,7 @@ const SpeechToText = () => {
                 {isRecording ? 'Stop Recording' : 'Start Recording'}
             </button>
             <div>
-                <strong>Transcript:</strong> {transcript}
+                <strong>Transcripts:</strong> {transcript}
             </div>
         </div>
     );
